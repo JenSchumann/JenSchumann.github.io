a2ba58043016e0dc269414b5c7f61c058123a78d
Jen Schumann <jenniferschumann1@gmail.com>
2017-07-19 05:46:24 -0600

    added wireframe directory and uploaded first wireframe png for this project

--- Projects/Project_One/Golf_Card_Game/golf.js/app.js
+++ Projects/Project_One/Golf_Card_Game/golf.js/app.js
@@ -1,6 +1,18 @@
-// console.log("let's play cards");
-(() => {
-// console.log('how about playing golf?');
+console.log("let's play cards");
+$(() => {
+console.log('how about playing golf?');
+
+// ============================================================================>
+
+// 2 Player Online Card Game Application: Golf
+
+let round = 0;
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+// console.log(round);
+// console.log(playerOneScore);
+// console.log(playerTwoScore);
 
 
 
@@ -28,6 +40,4 @@
 
 
 
-
-
-});
+})
