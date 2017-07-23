9ad0b34fe9ecfd3b3a2a1e0d1de8a71a9c12b87d
Jen Schumann <jenniferschumann1@gmail.com>
2017-07-23 09:42:44 -0600

    creating hi-lo game

--- Projects/Project_One/Nines_Alive/js_app/nines.js
+++ Projects/Project_One/Nines_Alive/js_app/nines.js
@@ -5,12 +5,23 @@ $(() => {
 // console.log('nines alive');
 
 let score = 0;
-let time = 30;
+let time = '';
 let round = 1;
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let value = 0;
 // let randNum = '';
 // let numberNine = 9;
+
+
+//some ideas for trying to create a game loop:
+//
+
+// another idea to update game to run according to readme:
+//  add up all showing numbers if there are any 9s unclicked when the timer reaches zero
+// win state is having positive number of points on the scoreboard, and lose state is having negative?
+
+// needs win state & loss state coding
+
 
 
 const createSquares = (NumberOfSquares) => {
@@ -29,65 +40,133 @@ const createSquares = (NumberOfSquares) => {
   }
 }
 
-// const calculateScore = (square) => {
-        // console.log(typeof square);
-      //   const valueOfSquare = applyRandomNumber.randNum.value;
-      //
-      //
-      //
-      //   // if(valueOfSquare !== ?????) {
-      //
-      //     score = score + randNum.value;
-      //     console.log('value errors are not happening but do i see value updating score yet');
-      //
-      // $('h1').text('Scoreboard: ' + score)
-    // }
+const applyRandomNumber = (square) => {
+            const randNum = Math.floor(Math.random() * 9) + 1
+            //call randNum to make sure it is working
+                      if(randNum === 1) {
+                        square.text(1).on('click', (e) => {
+                        score-=1
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 2) {
+                        square.text(2).on('click', (e) => {
+                        score-=2
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 3) {
+                        square.text(3).on('click', (e) => {
+                        score-=3
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 4) {
+                        square.text(4).on('click', (e) => {
+                        score-=4
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 5) {
+                        square.text(5).on('click', (e) => {
+                        score-=5
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 6) {
+                        square.text(6).on('click', (e) => {
+                        score-=6
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
+                      } else if(randNum === 7) {
+                        square.text(7).on('click', (e) => {
+                        score-=7
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
-const setTimer = () => {
-  const timer = setInterval(()=>{
-    time--
-    if(time === 0) {
-    // calculateScore();
-    clearInterval(timer);
-    // $('.squares').empty();
-    round++;
-  }
+                      } else if(randNum === 8) {
+                        square.text(8).on('click', (e) => {
+                        score-=8
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
-  $('#timer').text('Timer: ' + time + "s")
+                      } else if(randNum === 9) {
+                        square.text(9).on('click', (e) => {
+                        score+=9
+                        $('h1').text('Scoreboard: ' + score)
+                      })
 
-  }, 1000)
+                      }
 }
 
 
-const setUpRound = () => {
-      $('.squares').empty();
+const playRound = () => {
+          for(let i = 1; round.length < 10; i++) {
+          if (time === 0) {
+            $('.squares').empty();
+            setTimer();
+            setUpRound();
+            round++;
+          } else if (round > 9) {
+            alert ('stop this');
+            clearTimeout(setUpRound());
+            $('h1').text('Your final score is: ' + score)
+          // } else if ()
+          // prompt alert to play again?
+        }
+      }
+    }
 
-      $('#round').text('Round: ' + round);
 
-            if(round === 1){
-              createSquares(50);
-              time = 20;
-            } else if(round === 2) {
-              createSquares(100);
-              time = 20;
-            } else if(round === 3) {
-              createSquares(150);
-              time = 20;
-            } else if (round < 10) {
-              createSquares(250);
-              time = 20;
-            } else {
-              //needs a message alert that game is over
-              //win/loss status
-            }
+          const setTimer = () => {
+            const timer = setInterval(()=>{
+              time--
+              if(time === 0) {
+              clearInterval(timer);
+              playRound();
+              round++;
+              }
 
-}
+            $('#timer').text('Timer: ' + time + "s")
+
+              }, 1000)
+          }
+
+
+
+        const setUpRound = () => {
+            playRound();
+
+
+            $('.squares').empty();
+
+            $('#round').text('Round: ' + round);
+
+
+
+                  if(round === 1){
+                    createSquares(50);
+                    time = 20;
+                  } else if(round === 2) {
+                    createSquares(100);
+                    time = 20;
+                  } else if(round === 3) {
+                    createSquares(150);
+                    time = 20;
+                  } else if (round < 10) {
+                    createSquares(250);
+                    time = 20;
+                  } else  {
+                    //needs a message alert that game is over
+                    //win/loss status
+                  }
+      }
+
+// where should this functionality go?
+//else if statement taking values of all remaining unclicked 9s cause a sum of all other numbers to be subtracted from the score
+    // } else if (time === 0)
+
 
 // activating the begin button and load the squares
 
@@ -96,71 +175,11 @@ $('button').on('click', (e) => {
 
   setUpRound();
   setTimer();
+  playRound();
 
   })
 
-  const applyRandomNumber = (square) => {
-              const randNum = Math.floor(Math.random() * 9) + 1
-              //call randNum to make sure it is working
-                        if(randNum === 1) {
-                          square.text(1).on('click', (e) => {
-                          score-=1
-                          // let value = -1
-                          // score = score + this.value;
-                          // console.log('value errors are not happening but do i see value updating score yet');
-                          $('h1').text('Scoreboard: ' + score)
-                        })
 
-                        } else if(randNum === 2) {
-                          square.text(2).on('click', (e) => {
-                          score-=2
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 3) {
-                          square.text(3).on('click', (e) => {
-                          score-=3
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 4) {
-                          square.text(4).on('click', (e) => {
-                          score-=4
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 5) {
-                          square.text(5).on('click', (e) => {
-                          score-=5
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 6) {
-                          square.text(6).on('click', (e) => {
-                          score-=6
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 7) {
-                          square.text(7).on('click', (e) => {
-                          score-=7
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 8) {
-                          square.text(8).on('click', (e) => {
-                          score-=8
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        } else if(randNum === 9) {
-                          square.text(9).on('click', (e) => {
-                          score+=9
-                          $('h1').text('Scoreboard: ' + score)
-                        })
-
-                        }
-  }
 
 
 
---
+++ Projects/Project_One/hilow_card_game/js_dir/app.js
@@ -0,0 +1,84 @@
+$(() => {
+
+    // console.log('time to place your bets');
+    let myDollars = 100;
+    let cards = [];
+    const suits = ['spades', 'hearts', 'clubs', 'diams'];
+    //ace has highest value so it is placed last in the array
+    const numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+
+
+
+
+
+
+      const gameStart = () => {
+
+      const messageChange = $('#btnstart').on('click', (e) => {
+//when press start the message changes
+           $("#message").text("Game Started!");
+// after start button is pressed, it disappears
+           $(e.currentTarget).css('display', 'none');
+
+        })
+
+          $('#message').append(message);
+          buildCards();
+          shuffleArray(cards);
+}
+
+      const shuffleArray = (array) => {
+          for(let i = array.length -1; i>0; i--){
+            const holder = Math.floor(Math.random()* (i + 1));
+            // console.log(holder);
+            // console.log(array[i]);
+            // console.log(array[holder]);
+            //randomizing the array
+            const temp = array[i];
+            array[i] = array[holder];
+            array[holder] = temp;
+
+          }
+
+
+        return array;
+      }
+
+
+      const buildCards = () => {
+        //need to clear out array each time
+        cards = [];
+        for(s in suits){
+          for(n in numbers){
+              const card = {
+               suit: suits[s],
+               num: numbers[n]
+            }
+            cards.push(card)
+          }
+        }
+        console.log(cards);
+      }
+
+
+gameStart();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $('#btnstart').on('click', (e) => {
+      console.log('this is working');
+
+})
+
+  });
