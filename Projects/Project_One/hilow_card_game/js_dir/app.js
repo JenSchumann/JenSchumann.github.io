f77fa67b1271a0c61f014a9aa34ba92b4f1f42fa
Jen Schumann <jenniferschumann1@gmail.com>
2018-05-11 13:00:21 -0600

    updated game logic and functionality for hilo

--- Projects/Project_One/hilow_card_game/js_dir/app.js
+++ Projects/Project_One/hilow_card_game/js_dir/app.js
@@ -2,37 +2,44 @@ $(() => {
 
     // console.log('time to place your bets');
 
+//what is the game logic?
+// A player wins if they reach 5 points?
+// A player's round doesn't end until their 3 lives are out (They start the round w/3 lives and lose a life each time their bet it wrong?)
+// Could eventually post names & scores of top scorers?
 //==========================================================================
 // variables:
-let myDollars = 0;
+// let myDollars = 0;
 let cardOutput = document.getElementById('cards');
 let count = 0;
 let score = 0;
 let lives = 3;
 let cards = [];
+// var myDol = 0;
+
 const suits = ['spades', 'hearts', 'clubs', 'diams'];
 const numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-const $message = $('#message');
+const $message = $('#messageHL');
 let scoreOutput = document.getElementById('score');
+
+
 //==========================================================================
 
 const gameStart = () => {
-//
         const messageChange = $('#start').on('click', (e) => {
-  //when press start the message changes
-             $("#message").text("Game Started!");
-  // after start button is pressed, it disappears
-             $(e.currentTarget).css('display', 'none');
-             $('#highLow').css('display','block');
-        })
-        buildCards();
-        shuffleArray(cards);
+          //when press start the message changes
+          // console.log('start button clicked');
+          $("#messageHL").text("Game Started!");
+          // after start button is pressed, it disappears
+          $(e.currentTarget).css('display', 'none');
+          $('#highLow').css('display','inline-flex');
+          })
+          buildCards();
+          shuffleArray(cards);
           cardOutput.innerHTML +=showCard();
 }
 
 
 let hilo = (a) => {
-
             //calculate winner
             let win = false;
             let oldCard = cards[count].cardValue;
@@ -45,35 +52,58 @@ let hilo = (a) => {
                               if(a === 'high' && oldCard < newCard) {
                                 win = true;
                               } else if (a === 'low' && oldCard > newCard) {
-                                    win = true;
+                                win = true;
                               }
-                                      if(win) {
+                                    if(win) {
                                         score++;
-                                        $("#message").text("You were correct! Your score is now " + score + ".  LIVES remaining:  "+lives)
-
-                                        } else {
+                                        if(score == 5){
+                                          let cards = [];
+                                          winState();
+                                        } else if (win && score < 5){
+                                        $("#messageHL").text("You were correct! Score: " + score + " LIVES remaining:  "+lives);
+                                      }
+                                      } else {
                                           score--;
                                           lives--;
-                                        $("#message").text("You were wrong! Your score is now " + score + ".  LIVES remaining: " +lives--)
+                                        $("#messageHL").text("You were wrong! Your score is now " + score + ".  LIVES remaining: " +lives);
 
-                                              if(lives<1) {
-                                                endPlay();
-                                              } else if (score >= 5) {
-                                                $("#message").text("Congratulations!  You've reached 5 points!  You're a High-Roller now!")
+                                              if(lives == 0) {
+                                                let cards = [];
+                                                endPlay(score);
                                               }
-                                        scoreOutput.innerHTML = "SCORE: "+score+"LIVES: ("+lives+")";
                                         }
-}
+      }
 
 
-
-const endPlay = () => {
+const endPlay = (s) => {
+  //gets the nodelist of cards played in previous round:
+  // let lastCards = document.querySelectorAll(".icard");
+  // console.log(lastCards);
           document.getElementById('highLow').style.display='none';
-          $('#message').text("Game over!  Your score was " + score);
+          document.getElementById('score');
+          $('#messageHL').text("Game over!  Your score was " + s);
+          $('#start').css('display','block');
+          let count = 0;
+          let score = 0;
+          let lives = 3;
+          let cards = [" "];
+          gameStart();
 }
 
+const winState = () => {
 
+  document.getElementById('highLow').style.display='none';
+  document.getElementById('score');
+  $("#messageHL").text("Congratulations!  You've reached 5 points!  You're a High-Roller now! Press Start Game to play again!");
+  $('#start').css('display','block');
+  cardOutput.innerHTML = "";
+  let count = 0;
+  let score = 0;
+  let lives = 3;
+  let cards = [];
 
+  gameStart();
+}
 
 const shuffleArray = (array) => {
           for(let i = array.length -1; i>0; i--){
@@ -85,15 +115,11 @@ const shuffleArray = (array) => {
           return array;
 }
 
-
-
 let showCard = () => {
         let c = cards[count];
         let bgColor = (c.icon == "H" || c.icon == "D")?'red':'black';
           return '<span class="icard" style="color:'+bgColor+'">'+cards[count].num+'&'+ cards[count].suit+';</span>';
 }
-
-
 
 const buildCards = () => {
         cards = [];
@@ -110,28 +136,25 @@ const buildCards = () => {
             cards.push(card)
           }
         }
-        console.log(cards);
+        // console.log(cards);
       }
 
 
 // event listeners:
-
     $('#btnstart').on('click', (e) => {
-      console.log('this is working');
+      $("#cards").empty();
+      // console.log('this is working');
       gameStart();
     })
 
     $('#btnhigh').on('click', (e) => {
-      console.log('High button working');
+      // console.log('High button working');
       hilo('high');
-
     })
 
     $('#btnlow').on('click', (e) => {
-      console.log('Low button working');
+      // console.log('Low button working');
       hilo('low');
     })
 
-
-
-  });
+});
--- Projects/Project_One/hilow_card_game/readme.md
+++ Projects/Project_One/hilow_card_game/readme.md
@@ -18,3 +18,19 @@
 **PLANNED FEATURES:**
 -Add functionality for use of betting money from variable stored where the player begins game w/$100.
 -Improve responsiveness
+
+
+### **Problems I need to address w/HILO:**
+- [ ] buttons are not inline
+- [x] win state not working properly (when player gets 5 points they aren't receiving a win notification)
+- [ ] cards are not the same size
+- [ ] cards keep loading in same row even if screen is not in view
+- [ ] need to decide whether I'm going to incorporate gambling portion (& then what is the game logic I will use?) from the udemy class
+- [ ] need button options for playing again, cancel, etc.
+
+#### **Problem-solving approaches:**
+- 2 & 5 need to be solved w/javascript, game logic, after decision making
+- 1,3,4,6 will be solved by incorporating BS4
+
+
+CSS text shadow resource: https://designshack.net/articles/css/12-fun-css-text-shadows-you-can-copy-and-paste/
